e9b47c1152925bde56662cc129a8d569580df866
Dyan Carra <dyancarra@gmail.com>
2016-06-01 20:31:24 -0300

    fix

--- application/views/login/form.php
+++ application/views/login/form.php
@@ -1,3 +1,11 @@
+<? if(isset($message)): ?>
+  <div class="message">
+    <?= $message ?>
+  </div>
+<? endif; ?>
+
+<br />
+
 <form action="<?= base_url('/login/authenticate') ?>" method="post">
   <h1>login</h1>
 
@@ -16,3 +24,4 @@
   <input type="submit" value="Fazer Login" class="ui button blue full-size" /><br /><br />
   <a href="<?= base_url('/login/signup') ?>" class="ui button green full-size">Criar minha Conta</a>
 </form>
+
